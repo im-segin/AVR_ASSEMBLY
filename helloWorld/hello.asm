ac52a88fd27c7a1f646b9df6cc6092b53fbf5805
im-segin <segin.bytes@gmail.com>
2022-05-23 19:30:22 +0530

    .

--- helloWorld/hello.asm
+++ helloWorld/hello.asm
@@ -1,7 +1,9 @@
 .include "./m328Pdef.inc"
 
-        ldi r16,0b00000001
-        ldi r17,0b00000000
+;.org 0x0000
+
+        ldi r16,0b0000001
         out DDRB,r16
         out PORTB,r16
-        
+start:
+        rjmp start
