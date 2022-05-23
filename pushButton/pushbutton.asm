a76320e655c30807d20e0a02c56668d94b0299d5
im-segin <segin.bytes@gmail.com>
2022-05-23 22:48:34 +0530

    .

--- pushButton/pushbutton.asm
+++ pushButton/pushbutton.asm
@@ -2,24 +2,22 @@
 
 .include "m328Pdef.inc"
 .org 0x0000
-
 .def temp = r16
 rjmp voidsetup
 
 voidsetup:
-    ser temp                    ; set register with 0xff
+    ldi temp,0b00000001
     out DDRB,temp
     ldi temp,0b00000000
     out DDRD,temp
+    ldi temp,0b00000001
+    out PORTD,temp
     clr temp
     out PORTB,temp
-    ldi temp,0b000000001
-    out PORTD,temp
-
+    
 voidloop:
     in temp,PIND
     out PORTB,temp
     rjmp voidloop
-
 
 
